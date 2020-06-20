13fbb29092e5a60ab909e0fabe57ccb5a150a1c5
anis <anisshili.as@gmail.com>
2020-06-20 06:43:09 -0400

    task :  refactoring test file

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -3,20 +3,21 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
-    def test_instanciate_calcualtor(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
 
     def test_add(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(1, 6), 7)
-        self.assertEqual(calculator.result, 7)
+
+        self.assertEqual(self.calculator.add(1, 6), 7)
+        self.assertEqual(self.calculator.result, 7)
 
     def test_sub(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(5, 1), 4)
-        self.assertEqual(calculator.result, 4)
-
+        self.assertEqual(self.calculator.subtract(5, 1), 4)
+        self.assertEqual(self.calculator.result, 4)
 
 
 if __name__ == '__main__':
