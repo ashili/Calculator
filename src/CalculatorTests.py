f0c24ea3b4bce0db2eaa08f659a0fb0cba041e5c
anis <anisshili.as@gmail.com>
2020-06-20 11:57:11 -0400

    Test : added employee_birthday.txt for testing

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,4 +1,5 @@
 import unittest
+from pprint import pprint
 from Calculator import Calculator
 from CsvReader import CsvReader
 
@@ -7,15 +8,13 @@ class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.calculator = Calculator()
-        self.testData = CsvReader("")
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_add(self):
-
-        self.assertEqual(self.calculator.add(1, 6), 7)
-        self.assertEqual(self.calculator.result, 7)
+        testData = CsvReader('employee_birthday.txt').data
+        pprint(testData)
 
     def test_sub(self):
         self.assertEqual(self.calculator.subtract(5, 1), 4)
